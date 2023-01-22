e0d4491f23e69748955e6fc9ad09fe6d04b395eb
Eric Chasin <echasin@innvosolutions.com>
2023-01-21 18:09:24 -0800

    Initial Commit

---
+++ src/main/java/org/demo/ReactiveGreetingResource.java
@@ -0,0 +1,43 @@
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+import org.jboss.resteasy.reactive.RestPath;
+
+import io.quarkus.hibernate.reactive.panache.common.runtime.ReactiveTransactional;
+import io.smallrye.mutiny.Uni;
+
+@Path("/fruits")
+@ApplicationScoped
+public class ReactiveGreetingResource {
+
+    @GET
+    public Uni<List<Fruit>> listAll() {
+        return Fruit.listAll();
+    }
+
+    @GET
+    @Path("{id}")
+    public Uni<Fruit> findById(@RestPath Long id) {
+        return Fruit.findById(id);
+    }
+
+    @POST
+    @ReactiveTransactional
+    public Uni<Fruit> create(Fruit fruit) {
+        return Fruit.persist(fruit).replaceWith(fruit);
+    }
+
+    @DELETE
+    @Path("{id}")
+    @ReactiveTransactional
+    public Uni<Void> delete(@RestPath Long id) {
+        return Fruit.deleteById(id).replaceWithVoid();
+        
+        
+    }
+}
