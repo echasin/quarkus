b2b8db98aa6cd05d545fd93cc0bcb0559bcb03cc
Eric Chasin <echasin@innvosolutions.com>
2023-01-21 20:24:17 -0800

    Add filters

---
+++ src/main/java/org/demo/filter/EmployeeFilter.java
@@ -0,0 +1,11 @@
+package org.demo.filter;
+
+
+import lombok.Data;
+
+@Data
+public class EmployeeFilter {
+	private FilterField salary;
+	private FilterField age;
+	private FilterField position;
+}
